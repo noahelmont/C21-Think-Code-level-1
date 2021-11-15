8718226ecc3ae691a489b537e43473c1f41549a6
noah elmont <2047541@talnet.nl>
2021-11-15 10:28:10 +0100

    eind

---
+++ Opdracht5/Eindopdracht-thinkcode.py
@@ -0,0 +1,10 @@
+print("Hallo!")
+name = input("Wat is je naam? ")
+
+print("Leuk je te ontmoeten,", name)
+answer = input("vind je games leuk? ")
+
+if answer == "ja":
+    print("Goed om tee horen!")
+else:
+    print("Dat is jammer!")
